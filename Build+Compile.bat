c5727a4740e311f96e5af22f240e88f5920aad44
Christopher Jackson <cjackson@ameriben.com>
2018-06-15 08:43:57 -0600

    minor details

--- Build+Compile.bat
+++
@@ -1,7 +0,0 @@
-docker build -t better-youtube-sub:prod .
-
-docker build -t better-youtube-sub:dev --build-arg env=dev .
-
-docker run -p 4200:4200 --env-file ../dockerEnvVars.txt better-youtube-sub:prod
-
-docker run -p 4200:4200 --env-file ../dockerEnvVars.txt my-angular-project:dev
--- src/assets/fetchVideos.js
+++ src/assets/fetchVideos.js
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 const API_KEY = 'AIzaSyCBs5okQYwYL8XghpWFaf3YYKrBJUjnJi0';
 const PAGE_SIZE = 5;
 let subs = [];
